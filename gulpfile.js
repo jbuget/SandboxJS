940ddd83c2db38a37ba205092195ee218a2a01ba
Jérémy Buget <jbuget@octo.com>
2016-02-24 10:01:04 +0100

    Automation of build task

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var rev = require('gulp-rev');
 var sass = require('gulp-sass');
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
-var webpack = require('gulp-webpack');
+var zip = require('gulp-zip');
 
 var paths = {
     appJs: ['app/js/**/*.js', '!app/js/**/*.min.js'],
@@ -21,37 +21,63 @@ var paths = {
 };
 
 gulp.task('clean', function () {
-    return del(['build']);
+    return del(['target']);
 });
 
-gulp.task('usemin', function() {
-    return gulp.src('./*.html')
-        .pipe(usemin({
-            css: [ rev ],
-            html: [ function () {return minifyHtml({ empty: true });} ],
-            js: [ eslint, eslint.format, uglify, rev ],
-            inlinejs: [ uglify ],
-            inlinecss: [ minifyCss, 'concat' ]
-        }))
-        .pipe(gulp.dest('build/'));
-});
+/* ----------- */
+/* --- DEV --- */
+/* ----------- */
 
 gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass().on('error', gutil.log))
-        .pipe(gulp.dest('build/css'));
+        .pipe(gulp.dest('target/build/app/css'));
 });
 
 gulp.task('coffee', function() {
     gulp.src('app/coffee/**/*.coffee')
         .pipe(coffee({bare: true}).on('error', gutil.log))
-        .pipe(gulp.dest('build/js'));
+        .pipe(gulp.dest('target/build/app/js'));
 });
 
-gulp.task('webpack', function() {
-    return gulp.src(paths.appJs)
-        .pipe(webpack())
-        .pipe(gulp.dest('dist/'));
+gulp.task('build', ['sass', 'coffee'], function () {
+    gulp.src(['./*.html']).pipe(gulp.dest('target/build'));
+    gulp.src(['app/css/**/*.css']).pipe(gulp.dest('target/build/app/css'));
+    gulp.src(['app/img/*']).pipe(gulp.dest('target/build/app/img'));
+    gulp.src(['app/js/**/*.js']).pipe(gulp.dest('target/build/app/js'));
+    gulp.src(['bower_components/**/*']).pipe(gulp.dest('target/build/bower_components'));
 });
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/coffee/**/*.coffee', ['coffee']);
+});
+
+/* ------------ */
+/* --- DIST --- */
+/* ------------ */
+
+gulp.task('usemin', ['build'], function() {
+    return gulp.src('target/build/*.html')
+        .pipe(usemin({
+            css: [ rev ],
+            html: [ function () {return minifyHtml({ empty: true });} ],
+            js: [ uglify, rev ],
+            inlinejs: [ uglify ],
+            inlinecss: [ minifyCss, 'concat' ]
+        }))
+        .pipe(gulp.dest('target/dist'));
+});
+
+gulp.task('dist', ['usemin'], function() {
+    return gulp.src('target/dist/*')
+        .pipe(zip('sandboxjs.zip'))
+        .pipe(gulp.dest('target/dist'));
+});
+
+
+/* --------------- */
+/* --- DEFAULT --- */
+/* --------------- */
 
 gulp.task('default', ['usemin']);
